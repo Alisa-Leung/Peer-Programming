5eb9bf468ff4f219f14f159bb656fb5f9fdc50f0
Alisa Leung <alisal.singyee@gmail.com>
2025-10-11 22:36:28 -0700

    stuff

---
+++ project/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let interests = $state([]);
+    function update(){
+        interests.push(document.getElementById("interestsField").value);
+
+    }
+</script>
+
+<h1>Welcome to my page!</h1>
+<input type="text" id="interestsField"/>
+<button id="button" onclick={update}>Enter Interest</button>
+<p id="interestsParagraph">My interests are: {interests.reduce((a: string, b: string) =>a + b + ", ", "").slice(0, -2)}</p>
